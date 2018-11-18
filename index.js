d6a63f4b28ec4e7a197d0f20bbf8141e2d616eec
Solee Lee <carolinesoleelee@gmail.com>
2018-11-17 22:11:13 -0500

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+
+const newID = 1000000000 + oldID;
+const ageIsValid = Number.isInteger(currentAge);
+// Why did have to get this correct to get the first once correct too
+
+
+const randomNumber = Math.floor(Math.random() * 20) + 1;
+const randomInteger = Math.floor(randomNumber);
+const randomUserID = randomInteger + 1000000001;// Why add? Im confused?
